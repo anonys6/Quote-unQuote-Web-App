bd4a100c0f682a8e298a048fac38076808868c17
anonys6 <shakibalam998@gmail.com>
2023-12-28 13:48:18 +0530

    scratch done

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
-import { getDatabase, ref } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
 const appSetting = {
     databaseURL: "https://quote-unquote-ec289-default-rtdb.asia-southeast1.firebasedatabase.app/"
@@ -14,12 +14,53 @@ const btnPublish = document.getElementById("btn-publish");
 const endorsementInput = document.getElementById("input-endorsement");
 const endorsementList = document.getElementById("endorsement-list");
 
-btnPublish.addEventListener("click", function() {
-    endorsementList.innerHTML += `<li>${endorsementInput.value}</li>`
+btnPublish.addEventListener("click", function () {
+    let inputValue = endorsementInput.value;
+
+    push(endorsementListInDB, inputValue);
+    // endorsementList.innerHTML += `<li>${endorsementInput.value}</li>`
 
     clearInputField();
 })
 
+onValue(endorsementListInDB, function (snapshot) {
+    if (snapshot.exists()) {
+        let endorsementArray = Object.entries(snapshot.val());
+
+        clearEndorsementList();
+
+        for (let i = endorsementArray.length - 1; i >= 0; i--) {
+            let currentEndorsement = endorsementArray[i];
+            let currentEndorsementID = currentEndorsement[0];
+            let currentEndorsementValue = currentEndorsement[1];
+
+            appendEndorsementToEndorsementList(currentEndorsement);
+        }
+    } else {
+        endorsementList.innerHTML = "No endorsement here yet...";
+    }
+})
+
 function clearInputField() {
     endorsementInput.value = "";
+}
+
+function clearEndorsementList() {
+    endorsementList.innerHTML = "";
+}
+
+function appendEndorsementToEndorsementList(endorsement) {
+    let endorsementID = endorsement[0];
+    let endorsementValue = endorsement[1];
+
+    let newLi = document.createElement("li");
+    newLi.textContent = endorsementValue;
+
+    newLi.addEventListener("click", function () {
+        let exactLocationOfEndorsementInDB = ref(database, `endorsementList/${endorsementID}`);
+
+        remove(exactLocationOfEndorsementInDB);
+    })
+
+    endorsementList.append(newLi);
 }
